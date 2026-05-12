f9666ac901cd575af7a79b9f82fc0e797d43b300
green-yellw <110565845+green-yellw@users.noreply.github.com>
2026-05-12 10:55:25 +0100

    add a todo note in drink resource

--- godot/drink.gd
+++ godot/drink.gd
@@ -5,6 +5,8 @@ enum MainIngredient { NONE, COFFEE, TEA, CHAI }
 enum Liquid { NONE, WATER, MILK }
 enum Extra { NONE, SUGAR, ICE }
 
+# TODO: rename this to just 'drink' (couldnt before since Drink used to extend
+# Node which has a built-in property called name)
 @export var drink_name: String
 @export var main_ingredient: MainIngredient
 @export var liquid: Liquid
