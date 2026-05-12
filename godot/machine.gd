6a08bc35c6096c1d347e7a874e2930d1591b5a7f
green-yellw <110565845+green-yellw@users.noreply.github.com>
2026-05-12 20:01:45 +0100

    improve readability of machines

--- godot/machine.gd
+++ godot/machine.gd
@@ -4,17 +4,20 @@ extends Node3D
 @export var spot_for_customer: Marker3D
 @export var progress_bar: TextureProgressBar
 @export var timer: Timer
-@export var current_order_indicator: Label3D
+@export var customer_order_indicator: Label3D
 @export var final_order_indicator: Label3D
 @export var score_label: Label3D
 @export var accept_button: StaticBody3D
+@export var reject_button: StaticBody3D
+@export var waiting_approval_indicator: Label3D
+@export var making_drink_text: Label3D
 
 var occupied := false:
 	set(value):
 		occupied = value
 		if not occupied:
 			customer = null
-			current_order_indicator.hide()
+			customer_order_indicator.hide()
 			final_order_indicator.hide()
 			score_label.hide()
 			return
@@ -29,21 +32,26 @@ var waiting_for_response: bool = false
 func _ready() -> void:
 	timer.timeout.connect(_on_order_finished)
 	progress_bar.hide()
+	making_drink_text.hide()
 	score_label.hide()
-	current_order_indicator.hide()
+	customer_order_indicator.hide()
 	final_order_indicator.hide()
 
 
 func _physics_process(_delta: float) -> void:
 	progress_bar.value = (1 - timer.time_left / timer.wait_time) * 100
+	accept_button.visible = waiting_for_response
+	reject_button.visible = waiting_for_response
+	waiting_approval_indicator.visible = waiting_for_response
 
 
 func start_order() -> void:
 	timer.start()
 	customers_order = Global.drinks.pick_random()
-	current_order_indicator.text = "current order: " + customers_order.drink_name
-	current_order_indicator.show()
+	customer_order_indicator.text = "customer ordered: " + customers_order.drink_name
+	customer_order_indicator.show()
 	progress_bar.show()
+	making_drink_text.show()
 	print("starting order")
 
 
@@ -77,8 +85,9 @@ func score_drink() -> void:
 
 func _on_order_finished() -> void:
 	progress_bar.hide()
+	making_drink_text.hide()
 	completed_order = Global.drinks.pick_random()
-	final_order_indicator.text = "final order: " + completed_order.drink_name
+	final_order_indicator.text = "machine made: " + completed_order.drink_name
 	final_order_indicator.show()
 	waiting_for_response = true
 
@@ -87,15 +96,15 @@ func _on_accept_button_input_event(camera: Node, event: InputEvent, event_positi
 	print("Accept was clicked")
 	if(!waiting_for_response or completed_order == null):
 		return
-	
+	final_order_indicator.text = "order approved! \n dispensing drink to customer"
 	score_drink()
 	waiting_for_response = false
 	completed_order = null
-	
+
 	await get_tree().create_timer(randf_range(1, 2), false).timeout
 	Events.customer_left_machine.emit(customer)
 	occupied = false
-	
+
 	print("order finished")
 
 
@@ -103,7 +112,8 @@ func _on_reject_button_input_event(camera: Node, event: InputEvent, event_positi
 	print("Reject was clicked")
 	if(!waiting_for_response or timer.time_left > 0):
 		return
-	final_order_indicator.text = "final order: "
+	final_order_indicator.text = "order rejected! \n making a new drink"
 	timer.start()
 	progress_bar.show()
-	print()
+	making_drink_text.show()
+	waiting_for_response = false
