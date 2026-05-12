9378dc73086f8a28f57b11e5bce0a4701e3bf3f6
green-yellw <110565845+green-yellw@users.noreply.github.com>
2026-05-12 11:11:44 +0100

    fix typo
    lol

--- godot/drink.gd
+++ godot/drink.gd
@@ -5,7 +5,7 @@ enum MainIngredient { NONE, COFFEE, TEA, CHAI }
 enum Liquid { NONE, WATER, MILK }
 enum Extra { NONE, SUGAR, ICE }
 
-# TODO: rename this to just 'drink' (couldnt before since Drink used to extend
+# TODO: rename this to just 'name' (couldnt before since Drink used to extend
 # Node which has a built-in property called name)
 @export var drink_name: String
 @export var main_ingredient: MainIngredient
