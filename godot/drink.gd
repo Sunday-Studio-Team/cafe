82111cacb445265f86a518c4343b090cbddb38ee
green-yellw <110565845+green-yellw@users.noreply.github.com>
2026-05-18 22:29:40 +0100

    remove the todo comment about last commit

--- godot/drink.gd
+++ godot/drink.gd
@@ -5,8 +5,6 @@ enum MainIngredient { NONE, COFFEE, TEA, CHAI }
 enum Liquid { NONE, WATER, MILK }
 enum Extra { NONE, SUGAR, ICE }
 
-# TODO: rename this to just 'name' (couldnt before since Drink used to extend
-# Node which has a built-in property called name)
 @export var name: String
 @export var main_ingredient: MainIngredient
 @export var liquid: Liquid
