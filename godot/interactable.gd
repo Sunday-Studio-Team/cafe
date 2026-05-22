7ccb4a8975a3e6af6b29e798ed9e613e3dee9c78
green-yellw <110565845+green-yellw@users.noreply.github.com>
2026-05-22 23:58:46 +0100

    fix interactables re-enabling themselves

--- godot/interactable.gd
+++ godot/interactable.gd
@@ -9,11 +9,6 @@ signal interacted
 
 ## the name that will show in UI for this interactable
 @export var display_name: String
-## enabled to make this interactable disable itself after interacting
-@export var one_time_only := false
-## how long this interactable should be disabled after interacting
-## (if not onetimeonly)
-@export var lockout_length: float = 1.0
 ## mesh used for this object
 @export var mesh: MeshInstance3D
 ## if enabled, player has to HOLD interact to interact with this
@@ -67,11 +62,6 @@ func _physics_process(delta: float) -> void:
 		mesh.material_overlay.shader = Global.hover_shader
 
 
-# NOTE: all this lockout stuff is a bit untested tbh lol
-# (also probably a more elegant way to do things than this)
 func _on_interacted() -> void:
 	Global.hovered_interactable = null
 	enabled = false
-	if not one_time_only:
-		await get_tree().create_timer(lockout_length, false).timeout
-		enabled = true
