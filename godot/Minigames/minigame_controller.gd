2559c074d8d1e71613b4a9a7ec07b4a09e278c16
green-yellw <110565845+green-yellw@users.noreply.github.com>
2026-06-02 15:22:23 +0100

    set window settings, adjust minigame ui size + pos

--- godot/Minigames/minigame_controller.gd
+++ godot/Minigames/minigame_controller.gd
@@ -1,30 +1,21 @@
 extends CanvasLayer
-@onready var sub_viewport = $Control/SubViewportContainer/SubViewport
 
-#Minigame List 
-@export var minigame_dict : Dictionary[String, PackedScene]
-
+@export var minigame_dict: Dictionary[String, PackedScene]
+@export var sub_viewport: SubViewport
 
 
-#Adds the minigame to the subviewport so that it is playable
-func set_minigame(minigame_name : String):
-	var choosen_game : PackedScene = minigame_dict.get(minigame_name)
-	#If Null
+func play_minigame(minigame_name: String):
+	var choosen_game: PackedScene = minigame_dict.get(minigame_name)
+
+	# If Null
 	if not choosen_game:
 		print("This game does not exist")
-		#Put a generic "test game" as the default if it doesn't work
-	
-	#Adds game to the subviewport
+
 	sub_viewport.add_child(choosen_game.instantiate())
 
-#Turns on the minigame and turns on any related functions within the minigame. 
-func play_minigame(minigame_name : String):
-	#Sets the game
-	set_minigame(minigame_name)
-	
 	visible = true
 
-#Closes the game
+
 func close_game():
 	sub_viewport.get_child(0).queue_free()
 	visible = false
