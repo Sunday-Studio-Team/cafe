d257e588c5bd2dc86f2524a16a10d6f241beaf09
green-yellw <110565845+green-yellw@users.noreply.github.com>
2026-06-03 10:43:38 +0100

    add quit button
    since builds load in fullscreen now so players cant x out

--- godot/pause_menu.gd
+++ godot/pause_menu.gd
@@ -1,5 +1,11 @@
 extends CanvasLayer
 
+@export var quit_button: Button
+
+
+func _ready() -> void:
+	quit_button.pressed.connect(func(): get_tree().quit())
+
 
 func _physics_process(_delta: float) -> void:
 	if Input.is_action_just_pressed("pause") and not Global.minigame_active:
