42b2d8bf2cee730fb3543fbcb23b38e1a9ccaf23
green-yellw <110565845+green-yellw@users.noreply.github.com>
2026-06-13 18:06:50 +0100

    fix errors with sounds when reloading main scene

--- godot/ui/pc/ui.gd
+++ godot/ui/pc/ui.gd
@@ -232,10 +232,12 @@ func _on_score_updated(score_type: ScoreType, new_value: float, old_value: float
 		match score_type:
 			ScoreType.MONEY:
 				color = Color.GOLD
-				money_sound.play()
+				if is_inside_tree():
+					money_sound.play()
 			ScoreType.CUSTOMER:
 				color = Color.GREEN
-				gain_points_sound.play()
+				if is_inside_tree():
+					gain_points_sound.play()
 	else:
 		color = Color.RED
 		score_update_label.text = "-"
@@ -243,7 +245,8 @@ func _on_score_updated(score_type: ScoreType, new_value: float, old_value: float
 			ScoreType.MONEY:
 				pass
 			ScoreType.CUSTOMER:
-				lose_points_sound.play()
+				if is_inside_tree():
+					lose_points_sound.play()
 
 	score_update_label.modulate = color
 	match score_type:
