8ed2e2e20c3e10001b6f618debaae3d8b37c3b8a
green-yellw <110565845+green-yellw@users.noreply.github.com>
2026-06-29 19:12:54 +0100

    add more info to tablet ui

--- godot/ui/tablet/tablet_machine_ui.gd
+++ godot/ui/tablet/tablet_machine_ui.gd
@@ -2,6 +2,10 @@ class_name TabletMachineUI
 extends Control
 
 @export var timer_ui: TextureProgressBar
+@export var broken_down_text: Label
+@export var ready_text: Label
+@export var price_label: Label
+@export var rating_label: Label
 
 var machine: Machine
 
@@ -10,6 +14,21 @@ var machine: Machine
 
 func _physics_process(_delta: float) -> void:
 	if timer.is_stopped():
-		timer_ui.value = 0
+		timer_ui.hide()
 	else:
+		timer_ui.show()
 		timer_ui.value = (1 - timer.time_left / timer.wait_time) * 100
+
+	broken_down_text.visible = machine.broken_down
+
+	var waiting := machine.waiting_for_response
+	ready_text.visible = waiting
+	price_label.visible = waiting
+	rating_label.visible = waiting
+
+	if waiting:
+		price_label.text = "+%s" % Global.float_to_price(machine.order.made_drink.price)
+
+		var score := machine.order.score
+		rating_label.modulate = Color.GREEN if score > 0 else Color.RED
+		rating_label.text = "+%s🙂" % score if score > 0 else "%s🙂" % score
