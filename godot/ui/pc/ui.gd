8928706b03ed41202d4b260566f841f12aa0e854
green-yellw <110565845+green-yellw@users.noreply.github.com>
2026-06-29 10:59:05 +0100

    show shift length in ui before shift start

--- godot/ui/pc/ui.gd
+++ godot/ui/pc/ui.gd
@@ -178,8 +178,10 @@ func update_score_indicators() -> void:
 
 
 func update_time_indicator() -> void:
-	time_left_label.visible = not game_timer.is_stopped()
-	time_left_label.text = "TIME LEFT IN SHIFT: %ss" % int(game_timer.time_left)
+	if game_timer.is_stopped():
+		time_left_label.text = "SHIFT LENGTH: %ss" % int(game_timer.wait_time)
+	else:
+		time_left_label.text = "TIME LEFT IN SHIFT: %ss" % int(game_timer.time_left)
 
 
 func update_interactable_ui() -> void:
