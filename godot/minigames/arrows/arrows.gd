4393381270e4be749fa8be7e5d365c41ebaaeb35
shquam <87450533+shquam@users.noreply.github.com>
2026-07-04 15:05:09 -0400

    add input handling

--- godot/minigames/arrows/arrows.gd
+++ godot/minigames/arrows/arrows.gd
@@ -12,19 +12,34 @@ var blue_directions = []
 var red_directions = []
 var output_directions = []
 var valid_indices: Array[int] = []
+var input_to_direction: Dictionary = {
+	"move_left": "🡄",
+	"move_forward": "🡅",
+	"move_right": "🡆",
+	"move_back": "🡇",
+}
+var correct_input_index: int = 0
 
 @onready var background_color = "#" + background_panel.get_theme_stylebox("panel").get("bg_color").to_html(false)
 
+
 func _ready() -> void:
+	_start_minigame()
+
+
+func _start_minigame() -> void:
 	arrow_output.text = ""
 	var choose_color: float = randi_range(0, 1);
+	var blue_valid: bool
 	var color_choice: String
 	var text_color: String
 	# Choose if player has to click red or blue directions
 	if choose_color == 0:
+		blue_valid = true
 		text_color = blue
 		color_choice = "[bgcolor=%s][color=%s]blue[/color][/bgcolor]" % [background_color, text_color]
 	else:
+		blue_valid = false
 		text_color = red
 		color_choice = "[bgcolor=%s][color=%s]red[/color][/bgcolor]" % [background_color, text_color]
 	
@@ -38,26 +53,53 @@ func _ready() -> void:
 	# Insert arrows randomly into arrow_output, but chosen sequentially from each direction array
 	var bi: int = max_arrow_count/2 - 1
 	var ri: int = max_arrow_count/2 - 1
-	var blue_valid: bool
 	while bi >= 0 and ri >= 0:
 		var choose_dir = randi_range(0, 1);
 		if choose_dir == 0:
-			blue_valid = true
-			add_arrow(blue_directions, bi, true)
+			add_arrow(blue_directions, bi, blue_valid)
 			bi -= 1
 		else:
-			blue_valid = false
-			add_arrow(red_directions, ri, false)
+			add_arrow(red_directions, ri, !blue_valid)
 			ri -= 1
 	while bi >= 0:
-		add_arrow(blue_directions, bi, true)
+		add_arrow(blue_directions, bi, blue_valid)
 		bi -= 1
 	while ri >= 0:
-		add_arrow(red_directions, ri, false)
+		add_arrow(red_directions, ri, !blue_valid)
 		ri -= 1
 	
 	update_output()
 	print(valid_indices)
+
+
+func _end_minigame() -> void:
+	Events.minigame_end.emit()
+	print("end")
+
+
+func _input(event: InputEvent) -> void:
+	if event.is_pressed():
+		if event.is_action("move_left"):
+			check_input("move_left")
+		if event.is_action("move_forward"):
+			check_input("move_forward")
+		if event.is_action("move_right"):
+			check_input("move_right")
+		if event.is_action("move_back"):
+			check_input("move_back")
+	
+	if correct_input_index >= valid_indices.size():
+		_end_minigame()
+
+
+func check_input(action: String) -> void:
+	var valid_index: int = valid_indices[correct_input_index]
+	
+	if input_to_direction[action] == output_directions[valid_index][0]:
+		output_directions[valid_index][1] = background_color
+		correct_input_index += 1
+		update_output()
+
 
 # color_array can't be statically typed since Nested Type Collections are not supported. Code can be changed to use an array of classes instead.
 # build output_directions array, to use to update the output text later
