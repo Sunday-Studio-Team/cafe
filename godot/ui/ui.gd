c3695974b0ba21c69b6c3c9130c2619d8f772883
green-yellw <110565845+green-yellw@users.noreply.github.com>
2026-07-05 21:36:18 +0100

    rework employee rating in ui as stars

--- godot/ui/ui.gd
+++ godot/ui/ui.gd
@@ -23,6 +23,8 @@ enum ScoreType { MONEY, CUSTOMER }
 @export var shelf_item_name: RichTextLabel
 @export var shelf_item_description: RichTextLabel
 @export var day_indicator: Label
+@export var rating_stars_hbox: HBoxContainer
+@export var rating_goal_label: Label
 
 var score_update_tween: Tween
 var alert_tween: Tween
@@ -174,7 +176,54 @@ func update_score_indicators() -> void:
 		Global.float_to_price(Global.daily_profit)
 		+ " (goal: %s)" % Global.float_to_price(Stats.current.daily_profit_goal)
 	)
-	customer_happiness_label.text = "🙂" + str(Global.employee_rating) + " (goal: %s)" % Stats.current.employee_rating_goal
+
+	for c in rating_stars_hbox.get_children():
+		c.queue_free()
+
+	var current_rating := Global.employee_rating
+
+	var star_texture_rect := TextureRect.new()
+	star_texture_rect.texture = Global.star_texture
+	star_texture_rect.expand_mode = TextureRect.EXPAND_FIT_HEIGHT_PROPORTIONAL
+	star_texture_rect.stretch_mode = TextureRect.STRETCH_KEEP_ASPECT
+	star_texture_rect.custom_minimum_size = Vector2(50, 50)
+	star_texture_rect.size_flags_horizontal = Control.SIZE_SHRINK_BEGIN
+	star_texture_rect.size_flags_vertical = Control.SIZE_SHRINK_BEGIN
+
+	var half_star_texture_rect := TextureRect.new()
+	half_star_texture_rect.texture = Global.half_star_texture
+	half_star_texture_rect.expand_mode = TextureRect.EXPAND_FIT_HEIGHT_PROPORTIONAL
+	half_star_texture_rect.stretch_mode = TextureRect.STRETCH_KEEP_ASPECT
+	half_star_texture_rect.custom_minimum_size = Vector2(50, 50)
+	half_star_texture_rect.size_flags_horizontal = Control.SIZE_SHRINK_BEGIN
+	half_star_texture_rect.size_flags_vertical = Control.SIZE_SHRINK_BEGIN
+
+	var empty_star_texture_rect := TextureRect.new()
+	empty_star_texture_rect.texture = Global.empty_star_texture
+	empty_star_texture_rect.expand_mode = TextureRect.EXPAND_FIT_HEIGHT_PROPORTIONAL
+	empty_star_texture_rect.stretch_mode = TextureRect.STRETCH_KEEP_ASPECT
+	empty_star_texture_rect.custom_minimum_size = Vector2(50, 50)
+	empty_star_texture_rect.size_flags_horizontal = Control.SIZE_SHRINK_BEGIN
+	empty_star_texture_rect.size_flags_vertical = Control.SIZE_SHRINK_BEGIN
+
+	var rating_is_even := current_rating % 2 == 0
+	var rating_shown := 0
+
+	if rating_is_even:
+		for i in current_rating / 2.0:
+			rating_stars_hbox.add_child(star_texture_rect.duplicate())
+			rating_shown += 1
+	else:
+		for i in (current_rating - 1) / 2.0:
+			rating_stars_hbox.add_child(star_texture_rect.duplicate())
+			rating_shown += 1
+		rating_stars_hbox.add_child(half_star_texture_rect.duplicate())
+		rating_shown += 1
+
+	for i in 5 - rating_shown:
+		rating_stars_hbox.add_child(empty_star_texture_rect.duplicate())
+
+	rating_goal_label.text = "(goal: %s)" % (Stats.current.employee_rating_goal / 2)
 
 
 func update_time_indicator() -> void:
