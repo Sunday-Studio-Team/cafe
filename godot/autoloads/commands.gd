f44837561e00f141b851c8fbd4830831e3f2dd49
green-yellw <110565845+green-yellw@users.noreply.github.com>
2026-07-16 17:12:53 +0100

    add some info in console
    (most of the changes in console.gd are automatic from formatter lul, this was mostly just adding a command list that prints on startup)

--- godot/autoloads/commands.gd
+++ godot/autoloads/commands.gd
@@ -11,9 +11,28 @@ func _ready() -> void:
 		Console.enabled = false
 
 	# not sure whether to enable this or not, seems to break some stuff
-	# but might be better than accidentally pressing stuff in game by typing
+	# but might be better than accidentally pressing stuff in game by typing lol
 	#Console.pause_enabled = true
 	Console.font_size = 28
+	# dont think we need this yet, but if the commands list gets much longer itll go offscreen
+	#Console.toggle_size() # set fullscreen
+
+	# NOTE: theres a command which shows a command list but it seems to include builtin ones,
+	# so i think dumping this should help make this more friendly
+	Console.print_line(
+		"\n[b]COMMANDS[/b]
+- [i]startshift[/i] starts shift
+- [i]bank[/i] adds $100 to bank
+- [i]break[/i] makes a random machine break
+- [i]spill[/i] makes a random machine spill",
+	)
+	Console.print_line(
+		"\n[color=green]tip: try typing the start of a command and pressing TAB to autofill ![/color]",
+	)
+	Console.print_line(
+		"\n[color=gold](plz let us know what other commands you would find useful :D)
+(or tell us if any of the existing ones seem bugged D:)[/color]",
+	)
 
 	Console.add_command("startshift", start_shift)
 	Console.add_command("bank", bank)
