c99f76eaa2d7e16679bd8df257fba8ecd8c34308
green-yellw <110565845+green-yellw@users.noreply.github.com>
2026-07-17 01:34:47 +0100

    Revert "Random events 1" (#20)

--- godot/ui/pc/email_app_list_item.gd
+++ godot/ui/pc/email_app_list_item.gd
@@ -15,17 +15,15 @@ var email_data: EmailData
 var is_finished_important: bool
 var is_read: bool
 var is_current_day: bool
-var is_finished_spam: bool
 
 func _ready() -> void:
 	button.pressed.connect(_on_button_pressed)
 
-func initialize(init_email_data: EmailData, init_is_finished_important: bool, init_is_read: bool, init_is_current_day: bool, init_is_finished_spam: bool) -> void:
+func initialize(init_email_data: EmailData, init_is_finished_important: bool, init_is_read: bool, init_is_current_day: bool) -> void:
 	email_data = init_email_data
 	is_finished_important = init_is_finished_important
 	is_read = init_is_read
 	is_current_day = init_is_current_day
-	is_finished_spam = init_is_finished_spam
 	
 	sender_name_label.text = email_data.sender_name
 	subject_label.text = email_data.subject
@@ -34,11 +32,7 @@ func initialize(init_email_data: EmailData, init_is_finished_important: bool, in
 		displayed_date_time_label.text = email_data.displayed_time
 	else:
 		# Show day only
-		var days_ago = Global.day - email_data.day_to_send
-		if days_ago == 1:
-			displayed_date_time_label.text = "1 day ago"
-		else:
-			displayed_date_time_label.text = str(days_ago) + " days ago"
+		displayed_date_time_label.text = "x days ago"	
 	brief_contents_rich_text_label.text = email_data.contents
 	
 	_update_unread_indicator()
@@ -51,9 +45,6 @@ func mark_as_read() -> void:
 func mark_as_finished_important() -> void:
 	is_finished_important = true
 	_update_important_indicator()
-
-func mark_as_finished_spam() -> void:
-	is_finished_spam = true
 
 func _update_unread_indicator() -> void:
 	unread_indicator.visible = not is_read
