0a60f792942db231eb5bb287b64457b6ddea41bd
Ariana <thereihaveausername@gmail.com>
2026-07-23 00:59:43 -0400

    make new ingredient system work with captcha game

--- godot/minigames/captcha/ingredient_icon_holder.gd
+++ godot/minigames/captcha/ingredient_icon_holder.gd
@@ -8,16 +8,10 @@ enum Type { MAIN, LIQUID, EXTRA }
 @export var button: Button
 @export var button_disabled: bool
 
-var ingredient: int = 0:
+var ingredient: Ingredient = null:
 	set(value):
-		icon.texture = null
 		ingredient = value
-		if type == Type.MAIN:
-			icon.texture = Global.main_ingredient_icons.get(ingredient)
-		if type == Type.LIQUID:
-			icon.texture = Global.liquid_icons.get(ingredient)
-		if type == Type.EXTRA:
-			icon.texture = Global.extra_icons.get(ingredient)
+		icon.texture = ingredient.icon
 
 func _ready() -> void:
 	button.disabled = button_disabled
