65cbd1abdca5f7b031785aff4b91a12e9e52a191
Ariana <thereihaveausername@gmail.com>
2026-07-23 00:21:33 -0400

    add type to ingredient

--- godot/drinks/ingredient.gd
+++ godot/drinks/ingredient.gd
@@ -3,7 +3,9 @@ class_name Ingredient
 
 # NOTE: WRONG values for testing purposes
 enum Ingredient_Label { NONE, WRONG, COFFEE, GREEN_TEA, TEA, CHAI, WATER, MILK, SUGAR, ICE, ALMOND_MILK }
+enum Ingredient_Type { NONE, MAIN, LIQUID, EXTRA } # only used in minigame atm
 
 @export var icon: Texture2D
 @export var name: Ingredient_Label
+@export var type: Ingredient_Type
 @export var cost: float
