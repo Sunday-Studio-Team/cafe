6bf6e03ce80fe8f6539c796376aa5d540717c95f
green-yellw <110565845+green-yellw@users.noreply.github.com>
2026-07-23 09:08:40 +0100

    move input check for closing popups

--- godot/machine_interaction_popup.gd
+++ godot/machine_interaction_popup.gd
@@ -1,22 +1,25 @@
 extends CanvasLayer
+
 @export var popup_id: String = ""
+
 
 func _ready():
 	visible = false
 	Global.popups[popup_id] = self
-	
+
+
+func _physics_process(_delta: float) -> void:
+	if visible and Input.is_action_just_pressed("ui_cancel"):
+		close()
+
+
 func open() -> void:
 	visible = true
 	Global.popup_hint_showing = true
 	get_tree().paused = true
-	
+
+
 func close() -> void:
 	visible = false
 	Global.popup_hint_showing = false
 	get_tree().paused = false
-	
-func _unhandled_input(event):
-	if visible and event.is_action_pressed("ui_cancel"):
-		close()
-		get_viewport().set_input_as_handled()
-	
