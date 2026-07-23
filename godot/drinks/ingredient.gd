bb343e4f297a0fe81afd6b7d330d149b028d29fa
Ariana <thereihaveausername@gmail.com>
2026-07-23 00:32:30 -0400

    add new espresso bean ingredient

--- godot/drinks/ingredient.gd
+++ godot/drinks/ingredient.gd
@@ -2,7 +2,7 @@ extends Resource
 class_name Ingredient
 
 # NOTE: WRONG values for testing purposes
-enum Ingredient_Label { NONE, WRONG, COFFEE, GREEN_TEA, TEA, CHAI, WATER, MILK, SUGAR, ICE, ALMOND_MILK }
+enum Ingredient_Label { NONE, WRONG, COFFEE, ESPRESSO, GREEN_TEA, TEA, CHAI, WATER, MILK, SUGAR, ICE, ALMOND_MILK }
 enum Ingredient_Type { NONE, MAIN, LIQUID, EXTRA } # only used in minigame atm
 
 @export var icon: Texture2D
