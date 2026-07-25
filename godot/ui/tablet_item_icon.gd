d9cf3937ee09c038724c61ba691962dcf2601fe1
green-yellw <110565845+green-yellw@users.noreply.github.com>
2026-07-25 23:34:11 +0100

    bugs + details with item hover tooltip

--- godot/ui/tablet_item_icon.gd
+++ godot/ui/tablet_item_icon.gd
@@ -15,3 +15,16 @@ func _physics_process(_delta: float) -> void:
 		show()
 	else:
 		hide()
+
+	if Global.hovered_item_icon == self:
+		# this is just WHITE but with an intensity of 2
+		# dont ask
+		modulate = Color.from_hsv(0.0, 0.0, 1.825, 1.0)
+	else:
+		modulate = Color.WHITE
+
+	# to stop weird stuff where it might show popup in middle of screen
+	# if normal fps mouse mode moves the mouse there without triggering
+	# mouse exited signal
+	if Input.mouse_mode != Input.MOUSE_MODE_VISIBLE:
+		Global.hovered_item_icon = null
