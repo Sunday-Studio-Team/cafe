713841760d4d32c5e41e70bc365c50ab956e91bf
Ariana <thereihaveausername@gmail.com>
2026-07-24 21:33:04 -0400

    get similar ingredients on start

--- godot/drinks/ingredient.gd
+++ godot/drinks/ingredient.gd
@@ -9,6 +9,7 @@ enum Ingredient_Type { NONE, MAIN, LIQUID, EXTRA } # only used in minigame atm
 @export var name: Ingredient_Label
 @export var type: Ingredient_Type
 @export var cost: float
+var similar_ingredients: Array[Ingredient_Label] = [ ]
 
 func name_to_string() -> String:
 	match name:
@@ -38,3 +39,21 @@ func name_to_string() -> String:
 			return "almond milk"	
 		_:
 			return "unknown"
+
+func get_similar() -> void:
+	match Ingredient_Label:
+		Ingredient_Label.COFFEE:
+			similar_ingredients.append(Ingredient_Label.ESPRESSO)
+		Ingredient_Label.ESPRESSO:
+			similar_ingredients.append(Ingredient_Label.COFFEE)
+		Ingredient_Label.CHAI:
+			similar_ingredients.append(Ingredient_Label.TEA)
+		Ingredient_Label.TEA:
+			similar_ingredients.append(Ingredient_Label.CHAI)
+			similar_ingredients.append(Ingredient_Label.GREEN_TEA)
+		Ingredient_Label.GREEN_TEA:
+			similar_ingredients.append(Ingredient_Label.TEA)
+		Ingredient_Label.MILK:
+			similar_ingredients.append(Ingredient_Label.ALMOND_MILK)
+		Ingredient_Label.ALMOND_MILK:
+			similar_ingredients.append(Ingredient_Label.MILK)
