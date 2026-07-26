23ea5baf36a455f544fabe72a9aad995db7639b3
Ciaralooney <66528901+Ciaralooney@users.noreply.github.com>
2026-07-26 16:24:08 +0100

    speech bubbles popus done

--- godot/machine_interaction_popup.gd
+++ godot/machine_interaction_popup.gd
@@ -1,23 +1,28 @@
 extends CanvasLayer
-
 @export var popup_id: String = ""
 
+@onready var close_button: TextureButton = $InputBlocker/TextureButton
+
+const HOVER_COLOR := Color(0.7, 0.7, 0.7)
+const NORMAL_COLOR := Color(1, 1, 1)
 
 func _ready():
 	visible = false
 	Global.popups[popup_id] = self
+	close_button.pressed.connect(close)
+	close_button.mouse_entered.connect(_on_close_button_hover)
+	close_button.mouse_exited.connect(_on_close_button_unhover)
 
+func _on_close_button_hover() -> void:
+	close_button.modulate = HOVER_COLOR
 
-func _physics_process(_delta: float) -> void:
-	if visible and Input.is_action_just_pressed("ui_cancel"):
-		close()
-
-
+func _on_close_button_unhover() -> void:
+	close_button.modulate = NORMAL_COLOR
+	
 func open() -> void:
 	visible = true
 	Global.popup_hint_showing = true
 	get_tree().paused = true
-
 
 func close() -> void:
 	visible = false
