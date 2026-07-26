06ce4d70d0935aadb64214c7eb3869e8bd80dbe3
green-yellw <110565845+green-yellw@users.noreply.github.com>
2026-07-26 20:18:45 +0100

    add feature tag for skipping popups
    sorry for auto formatting

--- godot/machine_interaction_popup.gd
+++ godot/machine_interaction_popup.gd
@@ -1,10 +1,12 @@
 extends CanvasLayer
+
+const HOVER_COLOR := Color(0.7, 0.7, 0.7)
+const NORMAL_COLOR := Color(1, 1, 1)
+
 @export var popup_id: String = ""
 
 @onready var close_button: TextureButton = $InputBlocker/TextureButton
 
-const HOVER_COLOR := Color(0.7, 0.7, 0.7)
-const NORMAL_COLOR := Color(1, 1, 1)
 
 func _ready():
 	visible = false
@@ -13,18 +15,25 @@ func _ready():
 	close_button.mouse_entered.connect(_on_close_button_hover)
 	close_button.mouse_exited.connect(_on_close_button_unhover)
 
-func _on_close_button_hover() -> void:
-	close_button.modulate = HOVER_COLOR
 
-func _on_close_button_unhover() -> void:
-	close_button.modulate = NORMAL_COLOR
-	
 func open() -> void:
+	if OS.has_feature("skip_popups"):
+		return
+
 	visible = true
 	Global.popup_hint_showing = true
 	get_tree().paused = true
+
 
 func close() -> void:
 	visible = false
 	Global.popup_hint_showing = false
 	get_tree().paused = false
+
+
+func _on_close_button_hover() -> void:
+	close_button.modulate = HOVER_COLOR
+
+
+func _on_close_button_unhover() -> void:
+	close_button.modulate = NORMAL_COLOR
--- godot/tutorial/tutorial_screen_view.gd
+++ godot/tutorial/tutorial_screen_view.gd
@@ -5,9 +5,14 @@ signal finished(tutorial_screen_view: TutorialScreenView)
 
 @export var _continue_buttons: Array[TextureButton]
 
+
 func _ready() -> void:
 	for continue_button in _continue_buttons:
 		continue_button.pressed.connect(_on_continue_button_pressed)
+
+	if OS.has_feature("skip_popups"):
+		_on_continue_button_pressed()
+
 
 func _on_continue_button_pressed() -> void:
 	finished.emit(self)
