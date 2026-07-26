5eeb6a308d0c3cd4ccfcf3558c06ed14f074f2a0
green-yellw <110565845+green-yellw@users.noreply.github.com>
2026-07-26 20:06:36 +0100

    temp customer spawn/despawn sound

--- godot/customer.gd
+++ godot/customer.gd
@@ -8,6 +8,7 @@ const MOVE_SPEED := 2.0
 @export var waiting_bar: TextureProgressBar
 @export var timer: Timer
 @export var time_bonus_label: Label3D
+@export var spawn_sound: AudioStreamPlayer3D
 @export_dir var sprites_folder: String
 
 var desired_drink: Drink
@@ -33,6 +34,7 @@ func _ready() -> void:
 	desired_drink = Global.drinks.pick_random()
 
 	spawn_anim()
+	spawn_sound.play()
 
 
 func _physics_process(_delta: float) -> void:
@@ -95,6 +97,8 @@ func leave_store() -> void:
 	waiting_indicator.hide()
 	await move_to(Global.customer_leaving_spot.global_position)
 	await get_tree().create_timer(randf_range(1, 2), false).timeout
+	spawn_sound.pitch_scale = 4
+	spawn_sound.play()
 	await despawn_anim()
 	queue_free()
 
