04271fa3358ba50a842c16cd9a724f5d5b1ed3f5
Brazza <jjia@ufl.edu>
2026-08-01 00:28:17 -0400

    Special Shift
    There may be a special shift after the first day. It will make the game a bit different.

--- godot/autoloads/global.gd
+++ godot/autoloads/global.gd
@@ -15,6 +15,7 @@ extends Node
 @export var mop_cursor_texture: Texture2D
 @export var emails_schedule: Array[EmailData]
 @export var complaint_popup: CanvasLayer
+@export var special_shifts: Array[SpecialShift]
 
 var popups: Dictionary = { }
 var popup_hint_showing: bool = false
@@ -78,7 +79,7 @@ var employee_rating := 0:
 		if employee_rating < 0:
 			employee_rating = 0
 
-		# (see comment for same lines in above func)
+		# (see ccomment for same lines in above func)
 		await get_tree().process_frame
 		score_update_message = ""
 var bank_money := 0.0
@@ -95,6 +96,7 @@ var customer_sprites: Array[Texture]
 ## (tracked so we dont spawn 2 of the same)
 var customer_sprites_spawned: Array[Texture]
 var spill_sprites: Array[Texture]
+var current_special_shift: SpecialShift
 var breakdowns_this_shift := 0
 var spills_this_shift := 0
 var machines: Array[Machine]
---
+++ godot/special_shift/modification_data.gd
@@ -0,0 +1,6 @@
+# modification_data.gd
+class_name ModificationData
+extends Resource
+
+@export var modification: float = 0.0
+@export var multiply: bool = false
