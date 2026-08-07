2bfcaed651577497a872fb94e6d0531bcc1ac74e
green-yellw <110565845+green-yellw@users.noreply.github.com>
2026-08-07 23:56:06 +0100

    prevent dragging mop off screen in spill minigame

--- godot/minigames/spill_clean/mop.gd
+++ godot/minigames/spill_clean/mop.gd
@@ -1,4 +1,3 @@
-
 class_name DraggableMop
 extends Sprite2D
 
@@ -10,11 +9,11 @@ signal drag_ended
 
 var drag_collision: CollisionShape2D
 var drag_rectangle: RectangleShape2D
-
 var is_dragging: bool = false
 var drag_offset: Vector2 = Vector2.ZERO
+var is_wet: bool = false
 
-var is_wet: bool = false;
+@onready var mop_start_position: Vector2 = position
 
 
 func _ready() -> void:
@@ -55,12 +54,35 @@ func _ready() -> void:
 	bubbles.emitting = false
 	is_wet = false
 
+
+func _process(_delta: float) -> void:
+	if not is_dragging:
+		return
+
+	if (
+			not Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT)
+			or not mouse_is_in_viewport()
+	):
+		stop_dragging()
+		return
+
+	global_position = (
+			get_global_mouse_position()
+			+ drag_offset
+	)
+
+	# as a last resort, reset the mops position if its fully moved off screen
+	var viewport_rect := Rect2(Vector2.ZERO, get_viewport().get_visible_rect().size)
+	if not viewport_rect.has_point(position):
+		position = mop_start_position
+
+
 func _input(event: InputEvent) -> void:
 	if event is not InputEventMouseButton:
 		return
 
 	var mouse_event: InputEventMouseButton = (
-		event as InputEventMouseButton
+			event as InputEventMouseButton
 	)
 
 	if mouse_event.button_index != MOUSE_BUTTON_LEFT:
@@ -73,34 +95,25 @@ func _input(event: InputEvent) -> void:
 		stop_dragging()
 
 
-func _process(_delta: float) -> void:
-	if not is_dragging:
-		return
-
-	if not Input.is_mouse_button_pressed(
-		MOUSE_BUTTON_LEFT
-	):
-		stop_dragging()
-		return
-
-	global_position = (
-		get_global_mouse_position()
-		+ drag_offset
-	)
+# returns false if the mouse is out of bounds
+# (used to stop us dragging the mop off screen where we cant get it back)
+func mouse_is_in_viewport() -> bool:
+	var viewport_rect := Rect2(Vector2.ZERO, get_viewport().get_visible_rect().size)
+	return viewport_rect.has_point(get_viewport().get_mouse_position())
 
 
 func is_mouse_inside_drag_area() -> bool:
 	var local_mouse_position: Vector2 = (
-		drag_collision.get_local_mouse_position()
+			drag_collision.get_local_mouse_position()
 	)
 
 	var half_size: Vector2 = (
-		drag_rectangle.size * 0.5
+			drag_rectangle.size * 0.5
 	)
 
 	var inside: bool = (
-		absf(local_mouse_position.x) <= half_size.x
-		and absf(local_mouse_position.y) <= half_size.y
+			absf(local_mouse_position.x) <= half_size.x
+			and absf(local_mouse_position.y) <= half_size.y
 	)
 
 	print(
@@ -109,7 +122,7 @@ func is_mouse_inside_drag_area() -> bool:
 		" Half size: ",
 		half_size,
 		" Inside: ",
-		inside
+		inside,
 	)
 
 	return inside
@@ -122,8 +135,8 @@ func start_dragging() -> void:
 	is_dragging = true
 
 	drag_offset = (
-		global_position
-		- get_global_mouse_position()
+			global_position
+			- get_global_mouse_position()
 	)
 
 	drag_started.emit()
@@ -132,7 +145,6 @@ func start_dragging() -> void:
 func stop_dragging() -> void:
 	if not is_dragging:
 		return
-
 
 	is_dragging = false
 	drag_ended.emit()
