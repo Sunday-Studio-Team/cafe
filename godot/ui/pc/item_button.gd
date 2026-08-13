2f2f3b81f87544258d410d462e7196e3ad46e724
green-yellw <110565845+green-yellw@users.noreply.github.com>
2026-08-13 17:50:59 +0100

    add active indicator to items in shop
    plus some other small item/ui tweaks

--- godot/ui/pc/item_button.gd
+++ godot/ui/pc/item_button.gd
@@ -8,13 +8,16 @@ signal item_button_pressed(item_button: ItemButton)
 @export var item_icon: TextureRect
 @export var item_name: RichTextLabel
 @export var description: Label
+@export var active_indicator: Control
 
 var item: Item
+
 
 func _ready() -> void:
 	item_icon.texture = item.icon
 	item_name.text = "[b]%s [color=gold](%s)" % [item.name, Global.float_to_price(item.price)]
 	description.text = item.description
+	active_indicator.visible = item.is_active_item
 
 	pressed.connect(_on_button_pressed)
 
@@ -28,14 +31,15 @@ func set_up_tweens() -> void:
 		func():
 			var t := create_tween().set_parallel()
 			t.tween_property(self, "offset_transform_rotation", deg_to_rad(randf_range(-1, 1)), DUR)
-			t.tween_property(self, "offset_transform_position_ratio:y", -0.025, DUR)
+			t.tween_property(self, "offset_transform_position_ratio:y", -0.025, DUR),
 	)
 	mouse_exited.connect(
 		func():
 			var t := create_tween().set_parallel()
 			t.tween_property(self, "offset_transform_rotation", 0, DUR)
-			t.tween_property(self, "offset_transform_position_ratio:y", 0, DUR)
+			t.tween_property(self, "offset_transform_position_ratio:y", 0, DUR),
 	)
+
 
 func notify_pressed(did_buy_item: bool) -> void:
 	if did_buy_item:
