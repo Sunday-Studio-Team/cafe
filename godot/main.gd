bf85c5e56eee55de2d95fb4c7dd40a9b8efc68f6
nariloni <290497232+nariloni@users.noreply.github.com>
2026-08-14 23:14:03 +1000

    Make scene loading multithreaded, cache main scenes, and load some sub scenes separately

--- godot/main.gd
+++ godot/main.gd
@@ -302,7 +302,7 @@ func _apply_game_options(options_data: OptionsData) -> void:
 			_world_environment.environment.volumetric_fog_enabled = true
 			ProjectSettings.set_setting("rendering/global_illumination/gi/use_half_resolution", false)
 			ProjectSettings.set_setting("rendering/scaling_3d/scale", 1.0)
-			ProjectSettings.set_setting("rendering/anti_aliasing/quality/msaa_2d", RenderingServer.ViewportMSAA.VIEWPORT_MSAA_2X)
+			ProjectSettings.set_setting("rendering/anti_aliasing/quality/msaa_2d", RenderingServer.ViewportMSAA.VIEWPORT_MSAA_4X)
 			ProjectSettings.set_setting("rendering/anti_aliasing/quality/msaa_3d", RenderingServer.ViewportMSAA.VIEWPORT_MSAA_4X)
 		OptionsData.GraphicsOptionsPresets.MEDIUM:
 			_world_environment.environment.ssao_enabled = true
@@ -334,6 +334,8 @@ func _apply_game_options(options_data: OptionsData) -> void:
 		_:
 			pass
 	RenderingServer.gi_set_use_half_resolution(ProjectSettings.get_setting("rendering/global_illumination/gi/use_half_resolution"))
+	if not is_inside_tree():
+		await tree_entered
 	get_viewport().scaling_3d_scale = (ProjectSettings.get_setting("rendering/scaling_3d/scale") as float)
 	get_viewport().msaa_2d = (ProjectSettings.get_setting("rendering/anti_aliasing/quality/msaa_2d") as Viewport.MSAA)
 	get_viewport().msaa_3d = (ProjectSettings.get_setting("rendering/anti_aliasing/quality/msaa_3d") as Viewport.MSAA)
--- godot/scene_switcher.gd
+++ godot/scene_switcher.gd
@@ -16,10 +16,15 @@ const LOADING_FADE_OUT_TIME := 1.0
 @export var _main_scene_uid: StringName
 @export var _main_menu_uid: StringName
 @export var _end_of_day_dialog_scene_uid: StringName
+## Additional scenes to always keep cached for speed. 
+@export var _main_sub_scene_uids: Array[StringName]
 
 var current_scene: Node = null
 var loading_tween: Tween
 
+# Caching for quick loads
+var _cached_main_packed_scene: PackedScene
+var _cached_sub_packed_scenes: Dictionary[StringName, PackedScene]
 
 func _ready() -> void:
 	Events.scene_switch_requested.connect(load_scene)
@@ -36,8 +41,7 @@ func _process(_delta: float) -> void:
 
 
 func load_scene(scene: SceneSwitcher.GameScene) -> void:
-	var loading_start_time_ms: int = Time.get_ticks_msec()
-	print("SceneSwitcher: started timing loading")
+	const TIMING_PRINTS: bool = true
 	
 	if current_scene:
 		get_tree().paused = true
@@ -51,37 +55,96 @@ func load_scene(scene: SceneSwitcher.GameScene) -> void:
 	_loading_progress_bar.min_value = 0.0
 	_loading_progress_bar.max_value = 1.0
 
-	var scene_uid: StringName = _scene_enum_to_uid(scene)
-	var request_result: int = ResourceLoader.load_threaded_request(scene_uid)
-	if request_result != OK:
-		push_error("Failed to request scene.")
-		return
-	var progress_ratio_array: Array[float] = []
-	while ResourceLoader.load_threaded_get_status(scene_uid, progress_ratio_array) == ResourceLoader.ThreadLoadStatus.THREAD_LOAD_IN_PROGRESS:
-		if progress_ratio_array.size() > 0:
-			var progress_ratio: float = progress_ratio_array[0]
-			_loading_progress_bar.value = progress_ratio
-		await get_tree().process_frame
-	if ResourceLoader.load_threaded_get_status(scene_uid) != ResourceLoader.ThreadLoadStatus.THREAD_LOAD_LOADED:
-		push_error("Failed to load scene.")
-		return
-	var scene_resource: Resource = ResourceLoader.load_threaded_get(scene_uid)
-	if scene_resource == null:
-		push_error("Failed to get scene resource.")
-		return
-	var scene_packed_scene: PackedScene = scene_resource as PackedScene
-	if scene_packed_scene == null:
-		push_error("Error: scene resource is not a PackedScene")
-		return
+	var loading_start_time_ms: int = Time.get_ticks_msec()
+	if TIMING_PRINTS: print("SceneSwitcher: started timing loading")
+	
+	var cached_packed_scene: PackedScene = null
+	if scene == SceneSwitcher.GameScene.MAIN_SCENE:
+		if _cached_main_packed_scene != null:
+			cached_packed_scene = _cached_main_packed_scene
+
+	var scene_packed_scene: PackedScene = null
+	if cached_packed_scene != null:
+		scene_packed_scene = cached_packed_scene
+		if TIMING_PRINTS: print("SceneSwitcher: getting scene from cache")
+	else:
+		var scene_uid: StringName = _scene_enum_to_uid(scene)
+		
+		var scene_uid_requests: Array[StringName] = []
+		if scene == SceneSwitcher.GameScene.MAIN_SCENE:
+			scene_uid_requests.append_array(_main_sub_scene_uids)
+		scene_uid_requests.append(scene_uid)
+		
+		var finished_requests: int = 0
+		for scene_uid_request in scene_uid_requests:
+			var request_start_time_ms: int = Time.get_ticks_msec()
+			if TIMING_PRINTS: print("SceneSwitcher: started timing scene %s loading" % finished_requests)
+			
+			var use_sub_threads: bool = false
+			if scene_uid_request in _main_sub_scene_uids:
+				if TIMING_PRINTS: print("SceneSwitcher: loading sub scene with threads")
+				use_sub_threads = true
+			else:
+				if TIMING_PRINTS: print("SceneSwitcher: loading the main requested scene")
+				# WARNING: If set to true for the main scene, causes errors in the debugger, but doesn't seem to break anything yet: 
+					# E 0:00:02:675   get_script: /root/Global: The caller thread can't call the function `get_script()` on this node. Use `call_deferred()` or `call_deferred_thread_group()` instead.
+					#   <C++ Error>   Condition "!is_accessible_from_caller_thread()" is true. Returning: (Variant())
+					#   <C++ Source>  scene/main/node.cpp:4159 @ get_script()
+				# If it becomes a problem, set this to false!
+				use_sub_threads = true
+			
+			var request_result: int = ResourceLoader.load_threaded_request(scene_uid_request, "PackedScene", use_sub_threads)
+			if request_result != OK:
+				push_error("Failed to request scene.")
+				return
+			
+			var progress_ratio_array: Array[float] = []
+			while ResourceLoader.load_threaded_get_status(scene_uid_request, progress_ratio_array) == ResourceLoader.ThreadLoadStatus.THREAD_LOAD_IN_PROGRESS:				
+				if progress_ratio_array.size() != 1:
+					push_error("Progress ratio array empty?")
+					return
+				var progress_ratio: float = progress_ratio_array[0]
+				var total_progress_ratio: float = ((finished_requests as float) + progress_ratio) / (scene_uid_requests.size() as float)
+				_loading_progress_bar.value = total_progress_ratio
+				await get_tree().process_frame
+			
+			if ResourceLoader.load_threaded_get_status(scene_uid_request) != ResourceLoader.ThreadLoadStatus.THREAD_LOAD_LOADED:
+				push_error("Failed to load scene.")
+				return
+			var requested_scene_resource: Resource = ResourceLoader.load_threaded_get(scene_uid_request)
+			if requested_scene_resource == null:
+				push_error("Failed to get scene resource.")
+				return
+			var requested_packed_scene: PackedScene = requested_scene_resource as PackedScene
+			if requested_packed_scene == null:
+				push_error("Error: scene resource is not a PackedScene")
+				return
+			
+			if scene_uid_request == scene_uid:
+				scene_packed_scene = requested_packed_scene
+				if scene == SceneSwitcher.GameScene.MAIN_SCENE:
+					if TIMING_PRINTS: print("SceneSwitcher: cached main scene")
+					_cached_main_packed_scene = scene_packed_scene
+			elif scene_uid_request in _main_sub_scene_uids:
+				if TIMING_PRINTS: print("SceneSwitcher: cached sub scene")
+				_cached_sub_packed_scenes[scene_uid_request] = scene_packed_scene
+
+			var request_end_time_ms: int = Time.get_ticks_msec()
+			var request_total_duration_ms: int = request_end_time_ms - request_start_time_ms
+			if TIMING_PRINTS: print("SceneSwitcher: scene %s (%s) loading duration: %s ms" % [finished_requests, requested_packed_scene.resource_path, request_total_duration_ms])
+			
+			finished_requests += 1
+
+		if TIMING_PRINTS: print("SceneSwitcher: done loading, instantiating scene")
 
 	var loading_end_time_ms: int = Time.get_ticks_msec()
 	var loading_total_duration_ms: int = loading_end_time_ms - loading_start_time_ms
-	print("SceneSwitcher: total loading duration: %s ms" % loading_total_duration_ms)
+	if TIMING_PRINTS: print("SceneSwitcher: total loading duration: %s ms" % loading_total_duration_ms)
 	
 	_loading_progress_bar.visible = false
 
 	var instantiating_start_time_ms: int = Time.get_ticks_msec()
-	print("SceneSwitcher: started timing instantiation")
+	if TIMING_PRINTS: print("SceneSwitcher: started timing instantiation")
 
 	current_scene = scene_packed_scene.instantiate()
 	add_child(current_scene)
@@ -89,7 +152,7 @@ func load_scene(scene: SceneSwitcher.GameScene) -> void:
 	
 	var instantiating_end_time_ms: int = Time.get_ticks_msec()
 	var instantiating_total_duration_ms: int = instantiating_end_time_ms - instantiating_start_time_ms
-	print("SceneSwitcher: total instantiating duration: %s ms" % instantiating_total_duration_ms)
+	if TIMING_PRINTS: print("SceneSwitcher: total instantiating duration: %s ms" % instantiating_total_duration_ms)
 	
 	loading_tween = create_tween()
 	loading_tween.tween_property(loading_screen, "modulate:a", 0, LOADING_FADE_OUT_TIME).from(1)
