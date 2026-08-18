fd2de9a2ef3df19eb4519ba1c3fe29158135e748
nariloni <290497232+nariloni@users.noreply.github.com>
2026-08-18 11:40:45 +1000

    Adjust teleporter positions, and reset velocity on teleport

--- godot/teleporter.gd
+++ godot/teleporter.gd
@@ -13,6 +13,7 @@ func _on_body_entered(body: Node3D) -> void:
 	if body is CharacterBody3D:
 		body.global_position = destination.global_position
 		body.rotation.y = destination.rotation.y - deg_to_rad(90)
+		body.velocity = Vector3.ZERO
 		body.reset_physics_interpolation()
 
 
