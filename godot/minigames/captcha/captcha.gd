f91f79c08a446619017e3c44a3e9d63756dcd27d
green-yellw <110565845+green-yellw@users.noreply.github.com>
2026-08-22 00:58:47 +0100

    sfx + animation for captcha minigame

--- godot/minigames/captcha/captcha.gd
+++ godot/minigames/captcha/captcha.gd
@@ -11,6 +11,8 @@ extends SubViewportContainer
 @export var shake_intensity: float = 10
 @export var order_reminder: Control
 @export var click_sound: AudioStreamPlayer
+@export var correct_sound: AudioStreamPlayer
+@export var wrong_sound: AudioStreamPlayer
 
 var ordered_drink: Drink
 var main_text: String = "The required ingredients"
@@ -93,8 +95,24 @@ func verify_captcha() -> void:
 			)
 		):
 			shake_panel()
+			wrong_sound.play()
 			return
 
+	mouse_behavior_recursive = Control.MOUSE_BEHAVIOR_DISABLED
+	correct_sound.play()
+
+	for slot: IngredientIconHolder in captcha.get_children():
+		var scale_tween := create_tween()
+		scale_tween.tween_property(slot, "offset_transform_scale", Vector2.ONE * 0.75, 0.025)
+		scale_tween.tween_property(slot, "offset_transform_scale", Vector2.ONE, 0.025)
+
+		await get_tree().create_timer(0.025).timeout
+
+		var colour_tween := create_tween()
+		colour_tween.tween_property(slot, "modulate", Color.GOLD, 0.05)
+		colour_tween.tween_property(slot, "modulate", Color.WHITE, 0.05)
+
+	await correct_sound.finished
 	_end_minigame()
 
 
