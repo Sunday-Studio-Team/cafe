04f99cbc271e555589c27024db0aa533ea577471
shquam <87450533+shquam@users.noreply.github.com>
2026-08-22 11:49:29 -0400

    Update customer_container.gd

--- godot/minigames/captcha/customer_container.gd
+++ godot/minigames/captcha/customer_container.gd
@@ -5,6 +5,7 @@ extends CenterContainer
 func _can_drop_data(_at_position: Vector2, data: Variant) -> bool:
 	return data == finished_drink
 
+
 func _drop_data(_at_position: Vector2, _data: Variant) -> void:
 	root._end_minigame()
 	return
