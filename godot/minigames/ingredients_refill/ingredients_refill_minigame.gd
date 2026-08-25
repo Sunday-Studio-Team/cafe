c263a0e2e73f38f3a1e953c0da5418ea9fc01a50
noahturbs <nbt2016@gmail.com>
2026-08-25 12:34:24 -0400

    added assets to refill; revamped gameplay

--- godot/minigames/ingredients_refill/ingredients_refill_minigame.gd
+++ godot/minigames/ingredients_refill/ingredients_refill_minigame.gd
@@ -1,8 +1,10 @@
 extends Node2D
 
-const MOVE_SPEED := 6.7
-const NUM_BEANS_TO_SPAWN := 5
-const LEFT_RIGHT_FORCE := 250
+const MOVE_SPEED := 7.5
+const NUM_BEANS_TO_SPAWN := 8 #originally... 5 x0.4s
+const MAX_HORIZONTAL_BEAN_FORCE := 800
+const MAX_VERTICAL_BEAN_FORCE := 450
+
 
 @export var cup: CharacterBody2D
 @export var bean_scene: PackedScene
@@ -13,7 +15,8 @@ const LEFT_RIGHT_FORCE := 250
 @export var bean_spawn_timer: Timer
 @export var cup_area: Area2D
 @export var cup_boundaries: Area2D
-@export var meter: ProgressBar
+#@export var meter: ProgressBar
+@export var meter: TextureProgressBar
 @export var bag: Node2D
 @export var bean_hit_glasss_sound: AudioStreamPlayer2D
 @export var bag_shake_sound: AudioStreamPlayer2D
@@ -31,6 +34,7 @@ const LEFT_RIGHT_FORCE := 250
 
 var beans_in_cup: int = 0
 var beans_spawned: int = 0
+var gold_bean_spawned: bool = false #guarantees that the refill minigame can only spawn 1 gold bean.
 var bomb_bean_spawned: bool = false #guarantees that the refill minigame can only spawn 1 bomb bean.
 var screw_bean_spawned: bool = false #guarantees that the refill minigame can only spawn 1 screw bean.
 var bag_shake_tween: Tween
@@ -62,9 +66,10 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
+	
 	var left_right_input: float = Input.get_vector("move_left", "move_right", "move_forward", "move_back").x
 	cup.position.x += left_right_input * MOVE_SPEED
-	cup.position.x = clamp(cup.position.x, 600, 1300)
+	cup.position.x = clamp(cup.position.x, 350, 1920-350)
 	cup.rotation_degrees = lerp(
 		cup.rotation_degrees,
 		left_right_input * 7,
@@ -73,44 +78,98 @@ func _physics_process(delta: float) -> void:
 
 	meter.value = (beans_in_cup as float) / (NUM_BEANS_TO_SPAWN as float)
 
-func spawn_bean() -> void:
-	# let's... create a random int from 1-100
-	var random_int: int = randi_range(1,100)
-
-	if beans_spawned < NUM_BEANS_TO_SPAWN: #BEANS_TO_SPAWN decrements from ~5 to 0
-		var bean: RigidBody2D
-		if random_int<12 and bomb_bean_spawned == false:
-			bean = bomb_bean_scene.instantiate()
-			bomb_bean_spawned = true #guarantees that the refill minigame can only spawn 1 bomb  bean.
-
-		#elif random_int>=10 and random_int<20 and screw_bean_spawned == false:
-		#	bean = screw_bean_scene.instantiate()
-		#	screw_bean_spawned = true #guarantees that the refill minigame can only spawn 1 screw bean.
-
-		elif random_int>=20 and random_int<35 :
-			bean = golden_bean_scene.instantiate()
-		else: #we removed the screw, but rng for screw now goes to here.
-			bean = bean_scene.instantiate()
-
-		bean.global_position = pour_point.global_position
-		bean.add_to_group("beans")
-		add_child(bean)
-		bean.apply_impulse(Vector2(randf_range(-LEFT_RIGHT_FORCE, LEFT_RIGHT_FORCE), 0))
-		bean.rotation_degrees = randf_range(0, 360)
-		#bean.apply_torque_impulse(randf_range(-180,180)) #cant get this to work; spins the bean
-		beans_spawned += 1
-	else:
+func spawn_bean()->void:
+	
+	if beans_spawned >= NUM_BEANS_TO_SPAWN:
+		#we have too many beans. end minigame.
 		# all beans have spawned; start a timer to end the minigame (signal)
 		bean_spawn_timer.stop()
-
 		bag_shake_tween.kill()
 		bag_shake_sound.stop()
 		await get_tree().create_timer(1.5, false).timeout
-				
 		Global.refill_minigame_accuracy = meter.value
-		
 		Events.emit_signal("minigame_end")
+		print()
+		return
+	#create a random int from 1-100
+	var random_int: int
+	var bean: RigidBody2D
+	#first do a check for the golden bean
+	random_int =  randi_range(1,100)
+	#if(beans_spawned==0 and random_int<33): #33
+		#spawn_gold_bean(bean)
+	#else:
+		#spawn_normal_bean(bean)
+	#
+	#return
+	#
+	
+	if  beans_spawned< 3 and gold_bean_spawned == false : #gold bean case
+	
+		random_int =  randi_range(1,100)
+		if(beans_spawned==0 and random_int<33): #33
+			spawn_gold_bean(bean)
+		
+		elif(beans_spawned==1 and random_int<67): #67
+			spawn_gold_bean(bean)
+		elif(beans_spawned==2): #100
+			spawn_gold_bean(bean)
+		else:
+			spawn_normal_bean(bean)
+		return
+	else: #well, a golden bean has spawned in the past; decided between bomb and normal
+		random_int =  randi_range(1,100)
+		if (bomb_bean_spawned==true):
+			spawn_normal_bean(bean)
+		else:
+			
+			if random_int<(35 + beans_spawned *3):
+				spawn_bomb_bean(bean)
+			else:
+				spawn_normal_bean(bean)
 
+	return
+
+func spawn_normal_bean(bean:PhysicsBody2D) -> void:
+	bean = bean_scene.instantiate()			
+	#bean.scale= Vector2(2,2) #dont do this lol. rigidbodies will attempt to revert this.
+	bean.gravity_scale = 0.47
+	bean.global_position = pour_point.global_position
+	bean.add_to_group("beans")
+	add_child(bean)
+	bean.apply_impulse(Vector2(randf_range(-MAX_HORIZONTAL_BEAN_FORCE, -MAX_HORIZONTAL_BEAN_FORCE/2.8), randf_range( -MAX_VERTICAL_BEAN_FORCE,-MAX_VERTICAL_BEAN_FORCE/3)))
+	bean.rotation_degrees = randf_range(0, 360)
+	#bean.apply_torque_impulse(randf_range(-180,180)) #cant get this to work; spins the bean
+	beans_spawned += 1
+
+func spawn_gold_bean(bean:PhysicsBody2D) -> void:
+	gold_bean_spawned= true
+	bean = golden_bean_scene.instantiate()
+	bean.gravity_scale = 0.37 + randf_range(-0.17, 0.18)
+	bean.global_position = pour_point.global_position
+	bean.global_position.x-= 53
+	
+	bean.global_position.y-= 22 #adding a negative number, puts it vertically north. 
+	
+	bean.add_to_group("beans")
+	add_child(bean)
+	bean.apply_impulse(Vector2(randf_range(-MAX_HORIZONTAL_BEAN_FORCE+30, -MAX_HORIZONTAL_BEAN_FORCE/1.8), randf_range(-975,-940)))
+	bean.rotation_degrees = randf_range(0, 360)
+	#bean.apply_torque_impulse(randf_range(-180,180)) #cant get this to work; spins the bean
+	beans_spawned += 1
+
+func spawn_bomb_bean(bean:PhysicsBody2D) -> void:
+	bomb_bean_spawned= true
+	bean = bomb_bean_scene.instantiate()			
+	bean.is_bomb= true
+	bean.gravity_scale = 0.2
+	bean.global_position = pour_point.global_position
+	bean.add_to_group("beans")
+	add_child(bean)
+	bean.apply_impulse(Vector2(randf_range(-MAX_HORIZONTAL_BEAN_FORCE/1.5,-MAX_HORIZONTAL_BEAN_FORCE/2), -200))
+	bean.rotation_degrees = randf_range(0, 360)
+	#bean.apply_torque_impulse(randf_range(-180,180)) #cant get this to work; spins the bean
+	beans_spawned += 1
 
 func catch_bean(bean: PhysicsBody2D) -> void:
 	if not bean.is_in_group("beans"):
@@ -143,6 +202,7 @@ func spill_bean(bean: PhysicsBody2D) -> void:
 	if collected_beans.has(bean):
 		collected_beans.erase(bean)
 		beans_in_cup -= 1
+		#print('spill_bean has occured;', bean)
 
 
 func bomb() ->void:
@@ -152,8 +212,9 @@ func bomb() ->void:
 	var using_machine: Machine = Global.machine_in_use
 	if using_machine == null:
 		return
-
+	await get_tree().create_timer(0.15, false).timeout
 	bomb_sound_player.play()
+	#print('line 216. blastplayerfromusingmachine()')
 	using_machine.blast_player_from_using_machine()
 
 func screw():
@@ -176,13 +237,13 @@ func gold(bean: PhysicsBody2D):
 	beans_in_cup = NUM_BEANS_TO_SPAWN
 
 	#create a for loop; until 
-	while len(collected_beans)<5:
+	while len(collected_beans)<NUM_BEANS_TO_SPAWN:
 		collected_beans.append(bean)	
 
-	await get_tree().create_timer(0.5, false).timeout #has to be less than bean timer!
+	await get_tree().create_timer(0.5, false).timeout
 
 	Global.refill_minigame_accuracy = meter.value
-
+	#print("meter.value; we are in gold()",meter.value)
 	Events.emit_signal("minigame_end")
 
 
