b197fbea078068aa36dcd38f28fd367a46cce3cd
Ariana <thereihaveausername@gmail.com>
2026-08-24 22:59:53 -0400

    review prototype

---
+++ godot/emails/reviews/custom_email_view_reviews.gd
@@ -0,0 +1,14 @@
+extends Control
+class_name CustomEmailViewReview
+
+@export var review_1: Control
+@export var review_list: Control
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	#for drink: Drink in Global.drinks.filter(func(d: Drink): return d.day_unlocked == Global.day):
+		#var container = review_list.duplicate()
+		#container.drink = drink
+		#review_list.add_child(container)
+	# remove placeholder
+	review_list.remove_child(review_1) 	
---
+++ godot/emails/reviews/review.gd
@@ -0,0 +1,7 @@
+extends Resource
+class_name Review 
+
+@export var username: String
+@export var rating: float
+@export var review_content: String
+@export var sprite: Texture2D
