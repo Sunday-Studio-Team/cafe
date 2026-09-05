e090b3026ae47eec063d72330c2112dce023e7c8
mh859 <mh859@cornell.edu>
2026-09-05 19:33:19 -0400

    just using control nodes

--- godot/tutorial/tutorial_screen_view.gd
+++ godot/tutorial/tutorial_screen_view.gd
@@ -1,5 +1,6 @@
 class_name TutorialScreenView
 extends Control
+@onready var texture_rect: TextureRect = $TextureRect
 
 signal finished(tutorial_screen_view: TutorialScreenView)
 
@@ -7,6 +8,11 @@ signal finished(tutorial_screen_view: TutorialScreenView)
 
 
 func _ready() -> void:
+	var image = texture_rect.texture.get_image()
+	#var data = image.get_data()
+	#data.lock()
+	var pixel = image.get_pixel(1,2)
+	print(pixel)
 	for continue_button in _continue_buttons:
 		continue_button.pressed.connect(_on_continue_button_pressed)
 
