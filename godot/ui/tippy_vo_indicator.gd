ee421db3b29ef05eeaacef5dfddd1f330c20db19
green-yellw <110565845+green-yellw@users.noreply.github.com>
2026-09-07 19:27:15 +0100

    fix tippy vo indicator aspect ratio
    + always restart animation when indicator shows + minor script cleanup

--- godot/ui/tippy_vo_indicator.gd
+++ godot/ui/tippy_vo_indicator.gd
@@ -1,6 +1,5 @@
 extends CanvasLayer
 
-@export var vo_player: AudioStreamPlayer
 @export var sprite: AnimatedSprite2D
 
 var playing_last_frame := false
@@ -16,13 +15,14 @@ func _ready() -> void:
 	Global.voice_line_system.requested_hide_voice_line_subtitle.connect(_on_vo_finished)
 
 
-func _on_vo_started(voice_line: VoiceLine) -> void:
+func _on_vo_started(_voice_line: VoiceLine) -> void:
+	sprite.play("default")
 	var t := create_tween().set_parallel()
 	t.tween_property(sprite, "modulate", Color.WHITE, 0.25)
 	t.tween_property(sprite, "scale", sprite_starting_scale, 0.25)
 
 
-func _on_vo_finished(voice_line: VoiceLine) -> void:
+func _on_vo_finished(_voice_line: VoiceLine) -> void:
 	var t := create_tween().set_parallel()
 	t.tween_property(sprite, "modulate", Color.TRANSPARENT, 0.25)
 	t.tween_property(sprite, "scale", Vector2.ZERO, 0.25)
