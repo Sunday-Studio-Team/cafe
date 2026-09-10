4fa4b1f04d30e337cf4ce34ee3b924b1d8928c87
green-yellw <110565845+green-yellw@users.noreply.github.com>
2026-09-10 15:26:08 +0100

    fix bomb item not spawning, clean up scripts a bit

--- godot/exploding_bomb_on_player.gd
+++ godot/exploding_bomb_on_player.gd
@@ -1,53 +1,44 @@
 extends Node
 
-
 @export var exploding_bomb_scene: PackedScene
 @export var camera: Camera3D
 @export var player: Player
 
-var bomb_instance : Node3D 
+var bomb_instance: ExplodingBomb
 var has_exploding_bomb_item: bool = false
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	
+func _physics_process(delta: float) -> void:
 	handle_right_click(delta)
 
 
-
-
-
-
-func handle_right_click(delta: float)-> void:
-	#handles bomb spawning, logic w/ bomb timer
-	
-	if (has_item_checker() == false):
+# handles bomb spawning, logic w/ bomb timer
+func handle_right_click(_delta: float) -> void:
+	if not we_have_item():
 		return
-		
-	if (Input.is_action_just_pressed("right_click") ):
+
+	if (Input.is_action_just_pressed("right_click")):
 		if bomb_instance != null:
-			return #bomb exists. ignore right click.
-			
+			return  # theres already a bomb rn so dont do anything
+
 		else:
-			#else, we should plop down the bomb. grab where the player is pointing towards, and plop it down.
+			# else, we should plop down the bomb.
+			# grab where the player is pointing towards, and plop it down.
+			var throw_direction: Vector3 = -camera.global_transform.basis.z
+
 			bomb_instance = exploding_bomb_scene.instantiate()
-			#
-			#
-			var throw_direction = -camera.global_transform.basis.z
+			bomb_instance.global_position = camera.global_position + throw_direction
 			add_child(bomb_instance)
-			bomb_instance.global_position = camera.global_position + throw_direction / 2
-			bomb_instance.find_child("RigidBody3D").apply_impulse(throw_direction*6)
-	
-	pass
+
+			bomb_instance.apply_impulse(throw_direction * 6)
 
 
-func has_item_checker()->bool:
+func we_have_item() -> bool:
 	has_exploding_bomb_item = false
+
 	for item in Global.owned_items:
-		if item.name == "Exploding Bomb":
-			has_exploding_bomb_item=true
+		if item.item_id == "exploding_bomb":
+			has_exploding_bomb_item = true
+			break
 	return has_exploding_bomb_item
