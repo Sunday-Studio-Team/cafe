f27cb30cf7bc2c0ba3235ae3744f132088657d1a
nariloni <290497232+nariloni@users.noreply.github.com>
2026-09-11 05:57:06 +1000

    Move both machines in day 2 to left

--- godot/main.gd
+++ godot/main.gd
@@ -243,7 +243,7 @@ func set_per_day_stuff() -> void:
 	if Global.day == 2:
 		_active_machines.clear()
 		_active_machines.push_back(_left_area_right_machine)
-		_active_machines.push_back(_right_area_left_machine)
+		_active_machines.push_back(_left_area_left_machine)
 		_set_day_security_cameras_active([])
 
 	if Global.day == 3:
