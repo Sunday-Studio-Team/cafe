21ced25f2d8300286b602bc3db7f2588326fa980
Prickles <explodonator911@gmail.com>
2026-09-12 00:32:22 +0800

    panel changing color on ingredient correct/wrong

--- godot/order_breakdown_ui.gd
+++ godot/order_breakdown_ui.gd
@@ -7,12 +7,20 @@ enum Type { MAIN, LIQUID, EXTRA }
 @export var star_ui: Control
 @export var star_sign: Label
 @export var icon: TextureRect
+@export var style_array: Array[StyleBoxFlat]
+enum Style {
+	Default,
+	Correct,
+	Wrong
+}
 
 var ingredient: Ingredient = null:
 	set(value):
 		if value == null:
 			ingredient = null
 			icon.texture = null
+			remove_theme_stylebox_override("panel")
+			add_theme_stylebox_override("panel",style_array[Style.Default])
 		else:
 			ingredient = value
 			icon.texture = ingredient.icon
@@ -22,8 +30,12 @@ var correct: bool = false:
 		correct = value
 
 		if correct:
-			modulate = Color.GREEN
+			#modulate = Color.GREEN
+			remove_theme_stylebox_override("panel")
+			add_theme_stylebox_override("panel",style_array[Style.Correct])
 			star_sign.text = "+"
 		else:
-			modulate = Color.RED
+			#modulate = Color.RED
+			remove_theme_stylebox_override("panel")
+			add_theme_stylebox_override("panel",style_array[Style.Wrong])
 			star_sign.text = "-"
