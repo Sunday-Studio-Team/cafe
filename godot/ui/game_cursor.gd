99cdbac7f95f9ac7446f96cc106344a2eb748e74
green-yellw <110565845+green-yellw@users.noreply.github.com>
2026-09-12 22:55:13 +0100

    fix default OS cursor showing when dragging items
    (in loadout menu)

--- godot/ui/game_cursor.gd
+++ godot/ui/game_cursor.gd
@@ -5,18 +5,24 @@ extends Node
 
 @export var normal_pc_cursor: Texture2D
 @export var clicked_pc_cursor: Texture2D
-# global cursor function for all scenes involving a cursor
-# currently hotspot is set to (0,0) in 2d, so that's the top left hand corner
-# but can be changed later
+
 @export var normal_cursor_hotspot: Vector2 = Vector2.ZERO
 @export var pc_cursor_hotspot: Vector2 = Vector2.ZERO
 var _using_pc: bool
+
 
 func _ready() -> void:
 	Events.pc_state_change.connect(on_pc_state_change)
 
 	Input.set_custom_mouse_cursor(normal_cursor, Input.CURSOR_ARROW, normal_cursor_hotspot)
 	Input.set_custom_mouse_cursor(normal_cursor, Input.CURSOR_POINTING_HAND, normal_cursor_hotspot)
+	Input.set_custom_mouse_cursor(clicked_cursor, Input.CURSOR_CAN_DROP, normal_cursor_hotspot)
+	# the only place ive seen this 'forbidden' cursor is while dragging things in the item loadout menu,
+	# so i think setting it to the clicked cursor is good
+	# (but it might cause issues if that cursor ever gets set somewhere we're not supposed to be clicking . . .)
+	#	- jack
+	Input.set_custom_mouse_cursor(clicked_cursor, Input.CURSOR_FORBIDDEN, normal_cursor_hotspot)
+
 
 func _input(input_event: InputEvent) -> void:
 	if input_event is InputEventMouseButton and input_event.button_index == MOUSE_BUTTON_LEFT:
@@ -34,6 +40,7 @@ func _input(input_event: InputEvent) -> void:
 			else:
 				Input.set_custom_mouse_cursor(normal_cursor, Input.CURSOR_ARROW, normal_cursor_hotspot)
 				Input.set_custom_mouse_cursor(normal_cursor, Input.CURSOR_POINTING_HAND, normal_cursor_hotspot)
+
 
 func on_pc_state_change(using_pc: bool):
 	if using_pc:
