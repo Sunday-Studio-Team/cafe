344da0fb78f751616ca6ef2f57c993d77ef1865f
green-yellw <110565845+green-yellw@users.noreply.github.com>
2026-09-13 22:00:42 +0100

    add shift ending customer area music

---
+++ godot/customer_area_music.gd
@@ -0,0 +1,16 @@
+extends AudioStreamPlayer3D
+
+@export var shift_ending_music: AudioStream
+# NOTE: just playing thru the music once seems fine for now but we can loop this infinitely if needed
+@export var shift_ending_music_outro_loop: AudioStream
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	Events.shift_end_sequence_started.connect(
+			func() -> void:
+				stream = shift_ending_music
+				play()
+				autoplay = false
+	)
+
+
