cf35bda12bba1334d50e52e88c87366b6a8a6842
nariloni <290497232+nariloni@users.noreply.github.com>
2026-09-16 10:09:36 +1000

    Decrease VRAM use from 0.81GB to 0.75GB by only loading customer sprite UIDs when in use

--- godot/resources/customer_sprite_data.gd
+++ godot/resources/customer_sprite_data.gd
@@ -3,7 +3,34 @@ class_name CustomerSpriteData
 extends Resource
 
 @export var customer_name: String
-@export var sprite: Texture2D
+var sprite: Texture2D:
+	get:
+		return ResourceLoader.load(sprite_uid)
+@export var sprite_uid: StringName
+
+## Disable the preview if you're done with it, so it doesn't get loaded in game!
+@export var _editor_enable_preview_sprite: bool
+## Editor only. This will automatically update to give you a preview!
+## Adjust `sprite_uid`.
+@export var _editor_sprite: Texture2D:
+	get:
+		if !Engine.is_editor_hint():
+			return null
+		if not _editor_enable_preview_sprite:
+			return null
+		if sprite_uid == null:
+			return null
+		if not ResourceLoader.exists(sprite_uid):
+			return null
+		if _editor_sprite == null:
+			_editor_sprite = ResourceLoader.load(sprite_uid)
+		return _editor_sprite
+	set(value):
+		if !Engine.is_editor_hint():
+			_editor_sprite = null
+			return
+		_editor_sprite = value
+
 @export var alternate_desk_sprite: Texture2D
 var typing_minigame_portrait: Texture2D:
 	get:
@@ -53,11 +80,15 @@ var typing_minigame_portrait: Texture2D:
 			return
 		_editor_update_email_crop_texture()
 
+## Disable the preview if you're done with it, so it doesn't get loaded in game!
+@export var _editor_enable_preview_email_cropped_sprite: bool
 ## Editor only. This will automatically update to give you a preview!
 ## Adjust `email_crop_size_ratio` and `email_crop_center_ratio`.
 @export var editor_email_cropped_sprite_preview: AtlasTexture:
 	get:
 		if !Engine.is_editor_hint():
+			return
+		if not _editor_enable_preview_email_cropped_sprite:
 			return
 		if sprite == null:
 			return null
