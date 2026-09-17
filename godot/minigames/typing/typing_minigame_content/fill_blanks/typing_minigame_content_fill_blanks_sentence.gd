f36db11e91d8ba00411cd25f61b2c0f5105d09b1
nariloni <290497232+nariloni@users.noreply.github.com>
2026-09-18 04:07:48 +1000

    Fix some spaces in typing minigame content, and add IDs and editor preview to them

--- godot/minigames/typing/typing_minigame_content/fill_blanks/typing_minigame_content_fill_blanks_sentence.gd
+++ godot/minigames/typing/typing_minigame_content/fill_blanks/typing_minigame_content_fill_blanks_sentence.gd
@@ -1,5 +1,22 @@
+@tool
 class_name TypingMinigameContentFillBlanksSentence
 extends Resource
 
-@export var customer_dialog: String
+## Used only for easier identification with the external document we're manually importing from.
+@export var content_id: String 
+@export_tool_button("Force update preview") var _editor_force_update_preview_action: Callable = _editor_force_update_preview
+## Visual preview in editor only! Edit the `sentence_sections` content to update this.
+@export_multiline var _editor_sentence_sections_combined_preview: String:
+	get:
+		var combined_preview: String = ""
+		for sentence in sentence_sections:
+			if sentence is TypingMinigameContentFillBlanksSentencePrefilledSection:
+				combined_preview += sentence.prefilled_section
+			elif sentence is TypingMinigameContentFillBlanksSentenceTypedSection:
+				combined_preview += "\"%s\"" % sentence.typed_section
+		_editor_sentence_sections_combined_preview = combined_preview
+		return _editor_sentence_sections_combined_preview
 @export var sentence_sections: Array[TypingMinigameContentFillBlanksSentenceSection]
+
+func _editor_force_update_preview() -> void:
+	var _discard: String = _editor_sentence_sections_combined_preview
