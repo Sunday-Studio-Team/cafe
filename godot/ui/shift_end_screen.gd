f7ce375d740d53de2c971b2ef7c8d4f71fb68143
Prickles <explodonator911@gmail.com>
2026-09-24 14:13:11 +0800

    WIP endscreen (DOES NOT WORK YET)
    progress :)

--- godot/ui/shift_end_screen.gd
+++ godot/ui/shift_end_screen.gd
@@ -1,21 +1,9 @@
 extends CanvasLayer
 
 @export var background: ColorRect
-@export var _day_label: RichTextLabel
-@export var times_up: RichTextLabel
-@export var outcome: RichTextLabel
 @export var time_up_sound: AudioStreamPlayer
 @export var win_shift_sound: AudioStreamPlayer
 @export var lose_shift_sound: AudioStreamPlayer
-@export var _money_title_label: RichTextLabel
-@export var _min_profit_goal_label: RichTextLabel
-@export var _profit_made_label: RichTextLabel
-@export var _rating_title_label: RichTextLabel
-@export var _rating_label: RichTextLabel
-@export var _tips_per_star_label: RichTextLabel
-@export var _tip_jar_desc_label: RichTextLabel
-@export var _tips_today_label: RichTextLabel
-@export var _bank_total_label: RichTextLabel
 @export var bank_gain_sound: AudioStreamPlayer
 @export var pencil_scribble: AudioStreamPlayer
 @export var button: Button
@@ -28,8 +16,9 @@ extends CanvasLayer
 @export var earnings: RichTextLabel
 @export var new_ui_container: Control
 
-
-var value_to_show_on_bank_total: float
+var reached_bonus_rating: bool = (
+	Global.employee_rating >= Stats.current.item_bonus_rating_threshold
+)
 
 
 func _ready() -> void:
@@ -40,25 +29,10 @@ func _ready() -> void:
 			Events.end_screen_finished.emit(),
 	)
 
-	button.mouse_entered.connect(
-		func():
-			var t := create_tween().set_parallel()
-			t.tween_property(button, "offset_transform_scale", Vector2.ONE * 1.1, 0.1)
-			t.tween_property(button, "offset_transform_rotation", deg_to_rad(-1), 0.1),
-	)
-	button.mouse_exited.connect(
-		func():
-			var t := create_tween().set_parallel()
-			t.tween_property(button, "offset_transform_scale", Vector2.ONE, 0.1)
-			t.tween_property(button, "offset_transform_rotation", deg_to_rad(1), 0.1),
-	)
 	_on_time_up()
 
 
 func _process(_delta: float) -> void:
-	_bank_total_label.text = "🏦 Your Bank: [color=gold]%s[/color]" % Global.float_to_price(
-		value_to_show_on_bank_total
-	)
 
 	if visible:
 		Global.in_end_screen = true
@@ -68,18 +42,6 @@ func _process(_delta: float) -> void:
 
 func _on_time_up() -> void:
 	# Hide all the labels
-	_money_title_label.visible = false
-	_min_profit_goal_label.visible = false
-	_profit_made_label.visible = false
-	_day_label.visible = false
-
-
-	_rating_title_label.visible = false
-	_rating_label.visible = false
-	_tips_per_star_label.visible = false
-	_tip_jar_desc_label.visible = false
-	_tips_today_label.visible = false
-	_bank_total_label.visible = false
 	
 	cafe_profits.hide()
 	goal.hide()
@@ -88,14 +50,16 @@ func _on_time_up() -> void:
 	show()
 	background.show()
 	get_tree().paused = true
-	times_up.show()
+	#times_up.show()
 	time_up_sound.play()
 	await get_tree().create_timer(2).timeout
-	times_up.hide()
-	
+	#times_up.hide()
+	reached_bonus_rating = (
+		Global.employee_rating >= Stats.current.item_bonus_rating_threshold
+	)
 
 	# calculate everything
-	var daily_profit := 7 #Global.daily_cafe_money
+	var daily_profit := 67 #Global.daily_cafe_money
 	var min_profit_goal: float = 30 #Stats.current.daily_profit_goals_each_day[Global.day]
 	var passed_profit_goal := daily_profit >= min_profit_goal
 	animation_player.play("come_in_pass" if passed_profit_goal else "come_in_fail")
@@ -110,8 +74,6 @@ func _on_time_up() -> void:
 		#Global.float_to_price(daily_profit),
 		#Global.float_to_price(min_profit_goal),
 	#]
-	
-	_rating_label.text = "⭐ %s / %s" % [Global.employee_rating, Stats.current.employee_rating_max]
 	
 	pencil_scribble.play()
 	
@@ -148,42 +110,16 @@ func _on_time_up() -> void:
 			Global.daily_cafe_money >= Stats.current.daily_profit_goals_each_day[Global.day]
 	):
 		if Global.day == Global.final_day:
-			outcome.text = "[b][color=green]YOU WIN"
 			button.text = "end run"
 		else:
-			outcome.text = "[b][color=green]SHIFT CLEARED"
 			button.text = "continue"
 		win_shift_sound.play()
 	else:
-		outcome.text = "[b][color=red]FIRED"
 		button.text = "new run"
 		lose_shift_sound.play()
 
-	outcome.show()
-
 	await get_tree().create_timer(1.0).timeout
 
-	_rating_title_label.visible = true
-	await get_tree().create_timer(0.5).timeout
-	_rating_label.visible = true
-
-	await get_tree().create_timer(0.5).timeout
-
-	# If player has a free item slot,
-	# and if 5 star customer satisfaction, give free selection of 1 of 3 random items
-	# NOTE: disabled for now
-	#var has_free_item_slots: bool = Global.owned_items.size() <= Global.item_slots_amount
-	#var made_enough_money: bool = Global.daily_profit >= Stats.current.daily_profit_goal
-	#var five_star_rating: int = 10
-	#var got_five_stars: bool = Global.employee_rating >= five_star_rating
-	#if has_free_item_slots and made_enough_money and got_five_stars:
-	#var free_item_selector_screen: FreeItemSelectorScreen = _free_item_selector_screen_packed_scene.instantiate()
-	#if free_item_selector_screen == null:
-	#printerr("FreeItemSelectorScreen is null.")
-	#return
-	#_free_item_selector_screen_container.add_child(free_item_selector_screen)
-	#await free_item_selector_screen.finished_selection
-	#free_item_selector_screen.queue_free()
 	button.show()
 	await get_tree().create_timer(2).timeout
 	var button_shine_tween := create_tween().set_loops()
@@ -223,10 +159,6 @@ func grant_day_rewards(passed_day: bool) -> void:
 			current_day + 1
 		)
 		
-		var reached_bonus_rating: bool = (
-			Global.employee_rating >= Stats.current.item_bonus_rating_threshold
-		)
-	
 		var bonus_already_received: bool = (
 			SaveDataManager.save_data.days_bonus_objective_completed.get(current_day, false)
 		)
